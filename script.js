4181102a2d96422b2960b186e710d3ee1e62a0f8
welloliveiraoficial-cmyk <welloliveiraoficial@gmail.com>
2026-09-13 04:39:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const VAPID_KEY = "BFVSP3o_fL9b9qYzNwaHR3-DytRPRlmsPMoaBdbwT0uZOcUMd8ZCkUrfpVaZu
 const URL_NOTIFICAR_PUSH =
   "https://vercel.com/well-oliveira/cuidado-juntos-api";
 
-const CHAVE_API_PUSH = "SUBSTITUA_PELA_SUA_CHAVE_SECRETA";
+const CHAVE_API_PUSH = "cj9f8a2b7c1e4d6f0a3b5c8d1e2f4a6b";
 
 
 /* =========================================================
